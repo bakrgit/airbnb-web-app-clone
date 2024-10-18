95ecada2d513f0f84110133fdca7758c265c0923
bakrgit <201740593@fa-hists.edu.eg>
2024-10-18 15:46:35 +0300

    icons

--- shared-components/icons/index.ts
+++ shared-components/icons/index.ts
@@ -2,4 +2,6 @@ export { default as AddImageIcon } from "./add-image-icon";
 export { default as CloseIcon } from "./close-icon";
 export { default as EyeIcon } from "./eye-icon";
 export { default as ImageUploadIcon } from "./image-upload-icon";
+export { default as MinusIcon } from "./minus-icon";
 export { default as PlusIcon } from "./plus-icon";
+export { default as Plus } from "./plus";
